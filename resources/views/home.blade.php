9cdec3fbceff8703a66b5058c5dedd769df302ab
Troll Ádám <adam.troll@levoolabs.com>
2022-03-26 11:13:40 +0100

    Global layout implemented using Bootstrap.
    Home controller added. 
    Welcome page deleted.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('content')
+<h1 class="display-1">Home page</h1>
+<div>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum a reiciendis inventore maxime optio minima possimus tempore, ullam nesciunt expedita sapiente. Magnam minima vitae earum sint quae, quasi necessitatibus eos?</p>
+    <p>Quis tempore enim itaque molestiae! Adipisci, vel non, officia animi optio esse, dolorum accusamus neque quisquam quos eligendi excepturi cum sequi enim voluptate aliquam laudantium laboriosam similique ullam nam? Autem.</p>
+    <p>Voluptate voluptatum molestiae veniam voluptatem, quasi quia perferendis itaque ut fugiat illum, ullam laborum nobis non sed autem est a fugit minima vero accusantium dolorem reiciendis? Quam repudiandae tempora necessitatibus.</p>
+    <p>Aliquid, ad ab. Odio, quibusdam, hic, doloribus suscipit veniam quidem quasi maxime ad tenetur expedita quos. Ratione nobis similique optio, repudiandae et odio sapiente reiciendis id dolor quaerat beatae explicabo!</p>
+    <p>Vero atque officia, itaque culpa enim autem nemo debitis molestias accusantium quis totam laudantium minima neque maxime ad. Accusamus odit ullam rem sed provident repellat nemo vel impedit. Architecto, id.</p>
+    <p>Recusandae, saepe voluptatum consectetur magnam a necessitatibus sint voluptates hic. Totam adipisci dolores debitis eligendi pariatur incidunt voluptatibus perferendis corporis iusto veniam repudiandae sit voluptatem, suscipit ipsum at porro excepturi?</p>
+    <p>Atque adipisci ex non distinctio natus vitae unde vel tenetur expedita sint sit repudiandae quisquam, asperiores ratione quas minima, enim iste recusandae architecto nesciunt error omnis ab qui similique! Impedit.</p>
+    <p>Nulla obcaecati ipsam sequi consectetur, delectus veritatis labore fugit ipsum hic consequatur reiciendis placeat ad, aliquid quibusdam magnam fuga ullam incidunt sed impedit perspiciatis. Vero neque sit ipsum harum eos.</p>
+    <p>Tenetur dolorem quaerat natus accusamus dolor libero adipisci consequatur amet dicta est architecto, ea laudantium inventore, labore earum. Libero, aspernatur? Deserunt quod quibusdam impedit rem, illum eaque nemo? Molestias, aliquam.</p>
+    <p>Vero doloremque ut dolores pariatur animi eveniet amet iste, voluptatibus, beatae nobis fugiat, vel eos fuga exercitationem molestiae unde! Sequi velit harum magnam accusamus laborum accusantium corporis architecto sunt incidunt?</p>
+</div>
+@endsection
