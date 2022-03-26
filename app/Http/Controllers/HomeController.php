50fc3e006f9a1d78f3418ac872324f5d703ba44d
Troll Ádám <adam.troll@levoolabs.com>
2022-03-26 13:19:26 +0100

    Simple post listing added to the home page.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $posts = Post::all();
+        return view('home')->with('posts', $posts);
     }
 }
